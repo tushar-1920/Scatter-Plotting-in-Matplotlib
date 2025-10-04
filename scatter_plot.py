97ea50573676d1a2e5e800a2cc2d219949314323
Tushar <tusharmks9816@gmail.com>
2025-10-04 20:07:03 +0530

    Update scatter_plot.py

--- scatter_plot.py
+++ scatter_plot.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 # Study hours and exam scores for Class A
 class_a_hours = [2, 4, 6, 8]
 class_a_scores = [45, 95, 65, 85]
-v
+
 # Study hours and exam scores for Class B
 class_b_hours = [1, 3, 5, 7, 9]
 class_b_scores = [40, 50, 60, 70, 95]
